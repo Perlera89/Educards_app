8815aab9a22c002c971649bd2e774bec688d9a78
Manuel <69605277+Perlera89@users.noreply.github.com>
2022-10-23 16:07:15 -0600

    Agregando LoginActivity

---
+++ app/src/main/java/com/educard/educards_app/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.educard.educards_app.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.educards_app.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/educard/educards_app/view/SignUpActivity.kt
+++ app/src/main/java/com/educard/educards_app/view/SignUpActivity.kt
@@ -1,11 +1,12 @@
-package com.example.educards_app
+package com.educard.educards_app.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.educards_app.R
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
