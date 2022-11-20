e00e8330a9f2a3acf7aaf56fdfb63a87c8de37c3
Manuel <69605277+Perlera89@users.noreply.github.com>
2022-11-20 13:28:57 -0600

    Terminando AboutActivity

--- app/src/main/java/com/educards/activity/MainActivity.kt
+++ app/src/main/java/com/educards/activity/MainActivity.kt
@@ -1,18 +1,9 @@
 package com.educards.activity
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.*
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.viewpager.widget.*
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.tabs.TabLayout
 import android.os.Handler
 import android.os.Message
 import android.view.Menu
@@ -20,19 +11,26 @@ import android.view.MenuItem
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.*
 import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.*
 import com.bumptech.glide.Glide
 import com.educards.R
 import com.educards.adapter.FragmentAdapter
 import com.educards.fragment.DecksFragment
 import com.educards.fragment.FavoritesFragment
-import com.google.android.material.button.MaterialButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
     View.OnClickListener {
@@ -207,7 +205,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 Toast.makeText(this, "All favorites", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_about -> {
-                Toast.makeText(this, "About", Toast.LENGTH_SHORT).show()
+                val aboutIntent = Intent(this, AboutActivity::class.java)
+                startActivity(aboutIntent)
             }
             R.id.nav_donate -> {
                 Toast.makeText(this, "Donate", Toast.LENGTH_SHORT).show()
