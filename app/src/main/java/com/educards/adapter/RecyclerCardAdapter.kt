50a3ef4f8dcd2330dd010228c6e11bdab65c7bda
Wilian-Miranda <willymiranda1998@outlook.es>
2022-11-24 20:01:16 -0600

    Cambios a la forma de detactar la carta a activarle el girar
    -Ahora es necesario dar click en una tarjeta para girar
    -Evita  errores por el cambio de posicion del adapter position por los leves cambios a la derecha o izquierda

--- app/src/main/java/com/educards/adapter/RecyclerCardAdapter.kt
+++ app/src/main/java/com/educards/adapter/RecyclerCardAdapter.kt
@@ -2,22 +2,17 @@ package com.educards.adapter
 
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
-import android.app.Activity
-import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.educards.R
-import com.educards.model.Deck
 import com.educards.model.entities.Card
-import com.google.android.material.snackbar.Snackbar
 
 class RecyclerCardAdapter(private var cards: MutableList<Card>, private var _context:Context,private var _btReverse:ImageButton) :
     RecyclerView.Adapter<RecyclerCardAdapter.ViewHolder>() {
@@ -30,22 +25,19 @@ class RecyclerCardAdapter(private var cards: MutableList<Card>, private var _con
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val card = cards[position]
         holder.bind(card)
+        holder.itemView.setOnClickListener { holder.cardAnimation()}
+
     }
 
     override fun getItemCount(): Int {
         return cards.size
     }
 
-    override fun onViewAttachedToWindow(holder: ViewHolder) {
-        super.onViewAttachedToWindow(holder)
-        holder.cardAnimation()
-    }
-
-        inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
             private var question: TextView
             private var answer: TextView
-            private var cardQuestion: CardView
-            private var cardAnswer: CardView
+            private var cardQuestion: CardView?
+            private var cardAnswer: CardView?
 
             private lateinit var frontAnim: AnimatorSet
             private lateinit var backAnim: AnimatorSet
@@ -64,8 +56,8 @@ class RecyclerCardAdapter(private var cards: MutableList<Card>, private var _con
             }
             fun cardAnimation(){
                 val scale = _context.resources.displayMetrics.density
-                cardQuestion.cameraDistance = 8000 * scale
-                cardAnswer.cameraDistance = 8000 * scale
+                cardQuestion?.cameraDistance = 8000 * scale
+                cardAnswer?.cameraDistance = 8000 * scale
 
                 frontAnim = AnimatorInflater.loadAnimator(_context, R.animator.front_animation) as AnimatorSet
                 backAnim = AnimatorInflater.loadAnimator(_context, R.animator.back_animation) as AnimatorSet
